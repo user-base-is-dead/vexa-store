353b949bb15efaf7039b6f44b01b793a27bc5f15
Mishra <debasritmishra8@gmail.com>
2026-06-16 03:03:48 +0530

    Commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Search } from "../search/search";
 import { Cart } from "../cart/cart";
 import './Header.css';
--- frontend/src/components/search/search.jsx
+++ frontend/src/components/search/search.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './search.css'
 
 export const Search = () => {
