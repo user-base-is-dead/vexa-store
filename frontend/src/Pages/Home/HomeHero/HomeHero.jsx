b3b51dde10c2979a9935e17bdeb613ff9abe1473
Mishra <debasritmishra8@gmail.com>
2026-06-17 04:12:42 +0530

    Commit

--- frontend/src/Pages/Home/HomeHero/HomeHero.jsx
+++ frontend/src/Pages/Home/HomeHero/HomeHero.jsx
@@ -3,7 +3,24 @@ import './HomeHero.css'
 export const HomeHero = () => {
   return (
     <section className="hero">
-        <div className="container"></div>
+        <div className="container">
+          <div className="hero-content">
+            <h1 className="hero-title">
+              Premium Access
+              <br />
+              <em>for Every Budget</em>
+            </h1>
+
+            <p className="hero-text">
+              Your favorite subscriptions, shared smarter and priced lower.
+            </p>
+
+            <div className="hero-action">
+              <a href="/shop" className="shop-button">SHOP NOW</a>
+              <a href="/categories" className="categories-button">EXPLORE</a>
+            </div>
+          </div>
+        </div>
     </section>
   )
 }
